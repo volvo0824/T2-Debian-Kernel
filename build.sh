dc7d4d351cdbf98d8dd591c10f1f602909f1d234
Anders Fugmann <anders@fugmann.net>
2022-05-26 17:14:35 +0200

    Simplify build a bit

--- build.sh
+++ build.sh
@@ -12,118 +12,91 @@ fi
 KERNEL_VERSION=5.18.7
 PKGREL=1
 
-if [[ $USE_T2LINUX_REPO = true ]]
-then
-KERNEL_REPOSITORY=https://github.com/t2linux/kernel.git
-else
-#KERNEL_REPOSITORY=git://kernel.ubuntu.com/virgin/linux-stable.git
-KERNEL_REPOSITORY=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-fi
-
 APPLE_BCE_REPOSITORY=https://github.com/t2linux/apple-bce-drv.git
 APPLE_IBRIDGE_REPOSITORY=https://github.com/Redecorating/apple-ib-drv.git
 REPO_PATH=$(pwd)
-WORKING_PATH=${REPO_PATH}/build
-KERNEL_PATH=${WORKING_PATH}/linux-kernel
+WORKING_PATH=/tmp/build
+KERNEL_PATH=${WORKING_PATH}/linux-${KERNEL_VERSION}
 PACKAGE_PATH=${REPO_PATH}/packages
 
 ### Debug commands
 echo "Kernel version: ${KERNEL_VERSION}"
 echo "Working path: ${WORKING_PATH}"
-echo "Kernel repository: ${KERNEL_REPOSITORY}"
 echo "Current path: ${REPO_PATH}"
 echo "Package path: ${PACKAGE_PATH}"
 
 echo "CPU threads: $(nproc --all)"
 grep 'model name' /proc/cpuinfo | uniq
-
-get_next_version () {
-  echo $PKGREL
-}
-
 ### Clean up
-rm -rfv ./*.deb
-
+rm -fr *deb
+rm -fr ${WORKING_PATH}
 mkdir -p "${WORKING_PATH}" && cd "${WORKING_PATH}"
-rm -rf "${KERNEL_PATH}"
 
 ### Dependencies
+echo >&2 "===]> Info: Setup Debian... "
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y build-essential fakeroot libncurses-dev bison flex libssl-dev libelf-dev \
   openssl dkms libudev-dev libpci-dev libiberty-dev autoconf wget xz-utils git \
   libcap-dev bc rsync cpio debhelper kernel-wedge curl gawk dwarves zstd
-#apt install build-dep linux
+
+## apt build-dep linux
 
 ### get Kernel and Drivers
-if [[ $USE_T2LINUX_REPO = true ]]
-then
-git clone --depth 1 --single-branch --branch "t2-v${KERNEL_VERSION}" \
-  "${KERNEL_REPOSITORY}" "${KERNEL_PATH}"
-else
-git clone --depth 1 --single-branch --branch "v${KERNEL_VERSION}" \
-  "${KERNEL_REPOSITORY}" "${KERNEL_PATH}"
-fi
+echo >&2 "===]> Info: Obtain sources... "
+
+curl https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL_VERSION}.tar.xz | tar -C ${WORKING_PATH} -Jx
 git clone --depth 1 "${APPLE_BCE_REPOSITORY}" "${KERNEL_PATH}/drivers/staging/apple-bce"
 git clone --depth 1 "${APPLE_IBRIDGE_REPOSITORY}" "${KERNEL_PATH}/drivers/staging/apple-ibridge"
-cd "${KERNEL_PATH}" || exit
 
-if [[ $USE_T2LINUX_REPO = false ]]
-then
 #### Create patch file with custom drivers
-echo >&2 "===]> Info: Creating patch file... "
-KERNEL_VERSION="${KERNEL_VERSION}" WORKING_PATH="${WORKING_PATH}" "${REPO_PATH}/patch_driver.sh"
-fi
+echo >&2 "===]> Info: Updating patches... "
+mkdir -p ${WORKING_PATH}/patches
+cp -f ${REPO_PATH}/patches/*.patch ${WORKING_PATH}/patches
+WORKING_PATH="${WORKING_PATH}" ${REPO_PATH}/patch_driver.sh
 
-#### Apply patches
 cd "${KERNEL_PATH}" || exit
-
 echo >&2 "===]> Info: Applying patches... "
-[ ! -d "${REPO_PATH}/patches" ] && {
-  echo 'Patches directory not found!'
-  exit 1
-}
-
-
-while IFS= read -r file; do
+while read -r file; do
   echo "==> Adding $file"
-  patch -p1 <"$file"
-done < <(find "${REPO_PATH}/patches" -type f -name "*.patch" | sort)
-
-#chmod a+x "${KERNEL_PATH}"/debian/rules
-#chmod a+x "${KERNEL_PATH}"/debian/scripts/*
-#chmod a+x "${KERNEL_PATH}"/debian/scripts/misc/*
+  patch -p1 < "$file"
+done < <(find "${WORKING_PATH}/patches" -type f -name "*.patch" | sort)
 
 echo >&2 "===]> Info: Bulding src... "
 
-cd "${KERNEL_PATH}"
-make clean
+make -C ${KERNEL_PATH} clean
 
+echo >&2 "===]> Info: Update config... "
 # Copy the modified config
-cp "${REPO_PATH}/templates/default-config" "${KERNEL_PATH}/.config"
+cat "${REPO_PATH}/templates/default-config" | \
+    sed 's/CONFIG_VERSION_SIGNATURE=.*/CONFIG_VERSION_SIGNATURE=""/g' |
+    sed 's/CONFIG_SYSTEM_TRUSTED_KEYS=.*/CONFIG_SYSTEM_TRUSTED_KEYS=""/g' |
+    sed 's/CONFIG_SYSTEM_REVOCATION_KEYS=.*/CONFIG_SYSTEM_REVOCATION_KEYS=""/g' |
+    sed 's/CONFIG_DEBUG_INFO=y/# CONFIG_DEBUG_INFO is not set/g' |
+    sed 's/CONFIG_CONSOLE_LOGLEVEL_DEFAULT=.*/CONFIG_CONSOLE_LOGLEVEL_DEFAULT=4/g' |
+    sed 's/CONFIG_CONSOLE_LOGLEVEL_QUIET=.*/CONFIG_CONSOLE_LOGLEVEL_QUIET=1/g' |
+    sed 's/CONFIG_MESSAGE_LOGLEVEL_DEFAULT=.*/CONFIG_MESSAGE_LOGLEVEL_DEFAULT=4/g' > ${KERNEL_PATH}/.config
 
-# Make config friendly with vanilla kernel
-sed -i 's/CONFIG_VERSION_SIGNATURE=.*/CONFIG_VERSION_SIGNATURE=""/g' "${KERNEL_PATH}/.config"
-sed -i 's/CONFIG_SYSTEM_TRUSTED_KEYS=.*/CONFIG_SYSTEM_TRUSTED_KEYS=""/g' "${KERNEL_PATH}/.config"
-sed -i 's/CONFIG_SYSTEM_REVOCATION_KEYS=.*/CONFIG_SYSTEM_REVOCATION_KEYS=""/g' "${KERNEL_PATH}/.config"
-sed -i 's/CONFIG_DEBUG_INFO=y/# CONFIG_DEBUG_INFO is not set/g' "${KERNEL_PATH}/.config"
+echo >&2 "===]> Info: Make oldconfig... "
+make -C ${KERNEL_PATH} olddefconfig
 
-# I want silent boot
-sed -i 's/CONFIG_CONSOLE_LOGLEVEL_DEFAULT=.*/CONFIG_CONSOLE_LOGLEVEL_DEFAULT=4/g' "${KERNEL_PATH}/.config"
-sed -i 's/CONFIG_CONSOLE_LOGLEVEL_QUIET=.*/CONFIG_CONSOLE_LOGLEVEL_QUIET=1/g' "${KERNEL_PATH}/.config"
-sed -i 's/CONFIG_MESSAGE_LOGLEVEL_DEFAULT=.*/CONFIG_MESSAGE_LOGLEVEL_DEFAULT=4/g' "${KERNEL_PATH}/.config"
-
-make olddefconfig
+sed -i 's/CONFIG_DEBUG_INFO[ ]*=.*/# CONFIG_DEBUG_INFO is not set/g' ${KERNEL_PATH}/.config
 
 # Get rid of the dirty tag
-echo "" >"${KERNEL_PATH}"/.scmversion
+echo "" > ${KERNEL_PATH}/.scmversion
 
 # Build Deb packages
-make -j "$(getconf _NPROCESSORS_ONLN)" deb-pkg LOCALVERSION=-t2 KDEB_PKGVERSION="$(make kernelversion)-$(get_next_version)"
+make -C ${KERNEL_PATH} -j "$(getconf _NPROCESSORS_ONLN)" bindeb-pkg LOCALVERSION=-t2 KDEB_PKGVERSION="$(make kernelversion)-$PKGREL"
+
+ls -l ${WORKING_PATH}/*.deb
 
 #### Copy artifacts
 echo >&2 "===]> Info: Copying debs and calculating SHA256 ... "
 mkdir -p ${PACKAGE_PATH}
-cp -rfv "${KERNEL_PATH}/.config" "${PACKAGE_PATH}/kernel_config_${KERNEL_VERSION}"
-cp -rfv ../*.deb ${PACKAGE_PATH}
-sha256sum ../*.deb > ${PACKAGE_PATH}/sha256
+rm -f ${WORKING_PATH}/linux-image-*-dbg_*.deb
+cp -v ${KERNEL_PATH}/.config ${PACKAGE_PATH}/kernel_config_${KERNEL_VERSION}
+cp -v ${WORKING_PATH}/*.deb ${PACKAGE_PATH}
+sha256sum ${WORKING_PATH}/*.deb > ${PACKAGE_PATH}/sha256
+
+echo >&2 "===]> Info: Clean up build dir"
+rm -fr ${WORKING_PATH}
